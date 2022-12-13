47eebc8c9ad34fee49f1fe09969f4567ebf1ee1d
MUKESH KUMAR <66511917+Mukesh16407@users.noreply.github.com>
2022-12-13 16:02:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useContext } from "react";
+import "./styles.css";
+
+const ThemeContext = React.createContext("dark");
+
+function Comp1() {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <div>
+      <h1>Current Theme: {theme}</h1>
+    </div>
+  );
+}
+
+function Comp2() {
+  const { setTheme } = useContext(ThemeContext);
+  return (
+    <div>
+      <button onClick={() => setTheme("light")}>Light</button>
+      <button onClick={() => setTheme("dark")}>Dark</button>
+    </div>
+  );
+}
+
+export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={theme == "light" ? "light" : "dark"}>
+        <Comp1 />
+        <Comp2 />
+      </div>
+    </ThemeContext.Provider>
+  );
+}
